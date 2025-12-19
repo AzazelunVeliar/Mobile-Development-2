097615a92894d858d6edc47a1d6d222c30c4e400
AzazelunVeliar <144543807+AzazelunVeliar@users.noreply.github.com>
2025-12-19 15:17:02 +0300

    Lesson15

--- Lesson9/movieproject/src/main/java/ru/mirea/khudyakovma/movieproject/data/network/dto/ProductsResponseDto.java
+++
@@ -1,9 +0,0 @@
-package ru.mirea.khudyakovma.movieproject.data.network.dto;
-
-import com.google.gson.annotations.SerializedName;
-import java.util.List;
-
-public class ProductsResponseDto {
-    @SerializedName("products")
-    public List<ProductResponseDto> products;
-}
